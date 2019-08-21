001382232efa01035a3438ecd3848a68d0d9a547
mono21 <54351384+mono21@users.noreply.github.com>
2019-08-21 12:34:38 +0430

    Create InstallMTP.sh

---
+++ InstallMTP.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo Hello World
+# systemctl enable firewalld
+# systemctl start firewalld
+# yum -y install https://extras.getpagespeed.com/release-el7-latest.rpm 
+# yum -y install mtproxy
+# systemctl enable --now mtproxy
+# sudo firewall-cmd --zone=public --add-service=https --permanent
+# sudo firewall-cmd --reload
