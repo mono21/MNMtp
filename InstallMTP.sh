e2d60da740afa441b10f340f33c7ea6c7f5d529c
mono21 <54351384+mono21@users.noreply.github.com>
2019-08-21 13:16:55 +0430

    Update InstallMTP.sh

--- InstallMTP.sh
+++ InstallMTP.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-echo Hello World
-# systemctl enable firewalld
-# systemctl start firewalld
-# yum -y install https://extras.getpagespeed.com/release-el7-latest.rpm 
-# yum -y install mtproxy
-# systemctl enable --now mtproxy
-# sudo firewall-cmd --zone=public --add-service=https --permanent
-# sudo firewall-cmd --reload
+ systemctl enable firewalld
+ systemctl start firewalld
+ yum -y install https://extras.getpagespeed.com/release-el7-latest.rpm 
+ yum -y install mtproxy
+ systemctl enable --now mtproxy
+ sudo firewall-cmd --zone=public --add-service=https --permanent
+ sudo firewall-cmd --reload
+ cd /etc/mtproxy
+ rm -f mtproxy.params
+ touch mtproxy.params
+ chmod 0777 mtproxy.params
+ echo "MTPROXY_USER=mtproxy
+ MTPROXY_CLIENT_PORT=443
+ MTPROXY_STATS_PORT=8888
+ # Number of workers. You can increase the number of workers, if you have a powerful server
+ MTPROXY_WORKERS=1
+ # Any additional switches for daemon:
+ DAEMON_OPTS="-P de640994b8b171d8647de1ac6696df31"
+ " >> mtproxy.params
+ rm -f secret
+ touch secret
+ chmod 0777 secret
+ echo "MTPROXY_SECRET=00000000000000000000000000000000" >> secret
+ systemctl restart mtproxy
+ echo "COOL! Every Thing Work Fine Mohamad :)"
